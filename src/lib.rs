8106aed316640d003ea1d5ec455ca9eabc7e3611
Chris Foster <fostercm2@gmail.com>
2026-08-19 14:01:38 -0400

    remove incorrect code

--- src/lib.rs
+++ src/lib.rs
@@ -1095,11 +1095,6 @@ mod tests {
 
         println!("Connected to server at {}\n", server_addr);
 
-        let ep_clone = endpoint.clone();
-        let _ = tokio::spawn(async move {
-            let _ = ep_clone.read_cq().await;
-        });
-
         // Ping-pong loop
         let mut buf = vec![0u8; MESSAGE_SIZE];
 
